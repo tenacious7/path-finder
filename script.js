1e1479bff6a2f0ecf2a4e3715dad2f20f9fdc3eb
tenacious7 <137755716+tenacious7@users.noreply.github.com>
2023-11-07 23:14:17 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,111 +1,98 @@
-// Define your locations and paths data
-const locations = [
-    { name: "Main Gate", x: 100, y: 100 },
-    { name: "CRC-1", x: 200, y: 100 },
-    { name: "CRC-2", x: 300, y: 100 },
-    { name: "CSR Activity Center", x: 100, y: 200 },
-    { name: "Account Section", x: 150, y: 200 },
-    { name: "Temple", x: 50, y: 200 },
-    { name: "Dean's Office", x: 200, y: 50 },
-    { name: "Registrar's Office", x: 200, y: 80 },
-    // Add more locations as needed
-];
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <title>Custom Map</title>
+    <style>
+        /* Add your CSS styling here */
+        body {
+            text-align: center;
+            font-family: Arial, sans-serif;
+        }
+        h1 {
+            color: black;
+            font-weight: bold;
+        }
+        .location-inputs {
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            align-items: center;
+            margin: 20px 0;
+        }
+        .location-input {
+            margin: 10px 0;
+            padding: 5px;
+            border: 1px solid #ccc;
+            border-radius: 5px;
+        }
+        #findPathButton {
+            background-color: #007bff;
+            color: white;
+            border: none;
+            padding: 10px 20px;
+            border-radius: 5px;
+            cursor: pointer;
+            margin-top: 20px; /* Adjust the margin-top to center the button vertically */
+        }
+        .loading-bar {
+            display: none;
+        }
+        .loading-fill {
+            width: 0;
+            background-color: #007bff;
+            height: 10px;
+            border-radius: 5px;
+        }
+        /* Add CSS for the popup */
+        .popup {
+            position: fixed;
+            top: 50%;
+            left: 50%;
+            transform: translate(-50%, -50%);
+            background: white;
+            border: 1px solid #ccc;
+            box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
+            padding: 20px;
+            z-index: 1000;
+            text-align: center;
+        }
 
-const paths = [
-    {
-        from: "Main Gate",
-        to: "CRC-1",
-        steps: [
-            "Go straight from Main Gate to CRC-1.",
-            "Turn left ."
-        ]
-    },
-    {
-        from: "Main Gate",
-        to: "CRC-2",
-        steps: [
-            "Go straight from Main Gate towards CUTM logo  .",
-            "Turn right ."
-        ]
-    },
-    {
-        from: "Main Gate",
-        to: "CSR Activity Center",
-        steps: [
-            "Go straight after few step .",
-            "look towards Left.",
-            "Then you can find CSR hall board(farst floor)."
-        ]
-    },
-    {
-        from: "Main Gate",
-        to: "CSR Activity Center",
-        steps: [
-            "Go straight after few step .",
-            "look towards Left.",
-            "Then you can find CSR hall board(farst floor)."
-        ]
-    },
-    {
-        from: "Main Gate",
-        to: "CSR Activity Center",
-        steps: [
-            "Go straight after few step .",
-            "look towards Left.",
-            "Then you can find CSR hall board(farst floor)."
-        ]
-    },
-    {
-        from: "Main Gate",
-        to: "CSR Activity Center",
-        steps: [
-            "Go straight after few step .",
-            "look towards Left.",
-            "Then you can find CSR hall board(farst floor)."
-        ]
-    },
-    // Add more path directions with steps as needed
-];
-
-// Function to get step-by-step directions
-function getDirections(fromLocation, toLocation) {
-    // Search for a path from the 'fromLocation' to the 'toLocation'
-    const path = paths.find(path => path.from.toLowerCase() === fromLocation.toLowerCase() && path.to.toLowerCase() === toLocation.toLowerCase());
-
-    if (path) {
-        return path.steps;
-    } else {
-        return ["No directions found."]; // Handle the case when no path is found
-    }
-}
-
-// Function to display directions on the page
-function displayDirections(directions) {
-    const directionsContainer = document.getElementById("directions");
-    directionsContainer.innerHTML = directions.join("<br>");
-}
-
-// Event listener for the "Find My Path" button
-const findPathButton = document.getElementById("findPathButton");
-findPathButton.addEventListener("click", () => {
-    const fromLocation = document.getElementById("from").value;
-    const toLocation = document.getElementById("to").value;
-    
-    // Call the getDirections function and display the results
-    const directions = getDirections(fromLocation, toLocation);
-
-    if (directions[0] === "No directions found.") {
-        // If no path is found, display an emoji and a message in a pop-up
-        alert("😞 No directions found. Please check your input.");
-    } else {
-        // If directions are found, display them in a pop-up
-        alert(`🚀 Path Found!\n\nDirections:\n${directions.join("\n")}`);
-    }
-
-    // Clear the input fields
-    document.getElementById("from").value = "";
-    document.getElementById("to").value = "";
-
-    // Clear the directions container on the page
-    displayDirections([]);
-});
+        .popup button {
+            display: block;
+            margin: 10px auto;
+            background: #007bff;
+            color: white;
+            border: none;
+            padding: 10px 20px;
+            border-radius: 5px;
+            cursor: pointer;
+        }
+    </style>
+</head>
+<body>
+    <h1>Welcome to Centurion University Of Technology and Management</h1>
+    <!-- Add the image with the provided URL -->
+    <img src="https://th.bing.com/th/id/OIP.KYLzNfR7sqyhGUyTfhKdmAAAAA?pid=ImgDet&rs=1" alt="Your Custom Map Image" height="300px">
+    <div class="location-inputs">
+        <div class="location-input">
+            <label for="from">From:</label>
+            <input type="text" id="from" placeholder="Enter your current location">
+        </div>
+        <div class="location-input">
+            <label for="to">To:</label>
+            <input type="text" id="to" placeholder="Enter your destination">
+        </div>
+        <button id="findPathButton">Find My Path</button>
+    </div>
+    <div class="loading-bar">
+        <div class="loading-fill" id="loadingFill"></div>
+    </div>
+    <canvas id="customMap"></canvas>
+    <div id="directions"></div>
+    <script src="script.js"></script>
+    <link rel="stylesheet" type="text/css" href="style.css">
+</body>
+</html>
